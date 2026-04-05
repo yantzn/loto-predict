8f9bc4074e5ce545f5db39a2938d67963be268bd
shoya tezono <shoya.tezono@gmail.com>
2026-04-05 15:01:56 +0000

    feat: locals.tfにロト6およびロト7の検証ステージ用のテーブルIDを追加

--- infra/locals.tf
+++ infra/locals.tf
@@ -6,9 +6,11 @@ locals {
   }
 
   table_ids = {
-    loto6_history   = "loto6_history"
-    loto7_history   = "loto7_history"
-    prediction_runs = "prediction_runs"
-    execution_logs  = "execution_logs"
+    loto6_history          = "loto6_history"
+    loto7_history          = "loto7_history"
+    prediction_runs        = "prediction_runs"
+    execution_logs         = "execution_logs"
+    loto6_validation_stage = "loto6_validation_stage"
+    loto7_validation_stage = "loto7_validation_stage"
   }
 }
