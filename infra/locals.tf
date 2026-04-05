dd990a5840fa7fcd404dc9b9a173843ba0fef0cf
shoya tezono <shoya.tezono@gmail.com>
2026-04-05 14:51:09 +0000

    feat: 新しいlocals.tfファイルを追加し、共通ラベルとテーブルIDを定義

---
+++ infra/locals.tf
@@ -0,0 +1,14 @@
+locals {
+  common_labels = {
+    system     = "loto-predict"
+    managed_by = "terraform"
+    module     = "infra"
+  }
+
+  table_ids = {
+    loto6_history   = "loto6_history"
+    loto7_history   = "loto7_history"
+    prediction_runs = "prediction_runs"
+    execution_logs  = "execution_logs"
+  }
+}
